27c741573fcf66bc98255bb77d5234ef28a87dd0
kerev <backun-anatoly@yandex.ru>
2021-11-24 20:15:43 +0700

    creat generalPower

--- task_4.js
+++ task_4.js
@@ -1,4 +1,14 @@
 // task_4
+// расчет мощности включенных электроприборов
+const GeneralPower = {
+  power: 0,
+  sumPower: function (power) {
+    this.power += power;
+  },
+  totalPower: function () {
+    return `Общая мощность подключенная к сети: ${this.power} Вт`;
+  },
+};
 
 // электроприборы
 function ElectricalDevice() {
@@ -6,6 +16,7 @@ function ElectricalDevice() {
 }
 ElectricalDevice.prototype.plug_in = function (electricPlug) {
   if (electricPlug == "on") {
+    GeneralPower.sumPower(this.power);
     return "В розетку включен";
   }
   if (electricPlug == "off") {
@@ -17,11 +28,13 @@ ElectricalDevice.prototype.plug_in = function (electricPlug) {
 function Lighting(type_of, plinth, power) {
   this.type_of = type_of;
   this.plinth = plinth;
-  this.power = `${power} Вт`;
+  this.power = power;
 }
 Lighting.prototype = new ElectricalDevice();
 Lighting.prototype.plug_in = function (electricPlug) {
   if (electricPlug == "on") {
+    GeneralPower.sumPower(this.power);
+
     return "Лампа горит";
   }
   if (electricPlug == "off") {
@@ -34,11 +47,13 @@ function Kettle(volume, bodyMaterial, сolour, power) {
   this.volume = volume;
   this.bodyMaterial = bodyMaterial;
   this.colour = сolour;
-  this.power = `${power} Вт`;
+  this.power = power;
 }
 Kettle.prototype = new ElectricalDevice();
 Kettle.prototype.plug_in = function (electricPlug) {
   if (electricPlug == "on") {
+    GeneralPower.sumPower(this.power);
+
     return "Чайник включен";
   }
   if (electricPlug == "off") {
@@ -50,7 +65,7 @@ Kettle.prototype.plug_in = function (electricPlug) {
 function Iron(brand, weight, power) {
   this.brand = brand;
   this.weight = `${weight} кг`;
-  this.power = `${power} Вт`;
+  this.power = power;
 }
 Iron.prototype = new ElectricalDevice();
 
@@ -66,10 +81,4 @@ console.log(iron);
 console.log(iron.plug_in("on"));
 
 // подсчет мощности
-function power() {
-  //   if(при условии что прибор включен в розетку)
-  return `Cумарная мощность включенных электроприборов ${
-    parseInt(iron.power) + parseInt(kettle.power) + parseInt(lamp.power)
-  } Вт`;
-}
-console.log(power());
+console.log(GeneralPower.totalPower())
--- task_5.js
+++ task_5.js
@@ -1,9 +1,22 @@
+// task_5
+// расчет мощности включенных электроприборов
+const GeneralPower = {
+  power: 0,
+  sumPower: function (power) {
+    this.power += power;
+  },
+  totalPower: function () {
+    return `Общая мощность подключенная к сети: ${this.power} Вт`;
+  },
+};
+// электроприборы
 class ElectricalDevice {
   constructor() {
     this.voltage = 220;
   }
   plug_in(electricPlug) {
     if (electricPlug == "on") {
+      GeneralPower.sumPower(this.power);
       return "В розетку включен";
     }
     if (electricPlug == "off") {
@@ -18,10 +31,11 @@ class Lighting extends ElectricalDevice {
     this.brand = brand;
     this.type_of = type_of;
     this.plinth = plinth;
-    this.power = `${power} Вт`;
+    this.power = power;
   }
   plug_in(electricPlug) {
     if (electricPlug == "on") {
+      GeneralPower.sumPower(this.power);
       return "Лампа горит";
     }
     if (electricPlug == "off") {
@@ -36,10 +50,12 @@ class Kettle extends ElectricalDevice {
     this.brand = brand;
     this.volume = volume;
     this.bodyMaterial = bodyMaterial;
-    this.power = `${power} Вт`;
+    this.power = power;
   }
   plug_in(electricPlug) {
     if (electricPlug == "on") {
+      GeneralPower.sumPower(this.power);
+
       return "Чайник включен";
     }
     if (electricPlug == "off") {
@@ -53,7 +69,7 @@ class Iron extends ElectricalDevice {
     super(voltage);
     this.brand = brand;
     this.weight = weight;
-    this.power = `${power} Вт`;
+    this.power = power;
   }
 }
 
@@ -66,24 +82,7 @@ console.log(kettle);
 console.log(iron);
 
 console.log(lamp.plug_in("on"));
-console.log(kettle.plug_in("off"));
+console.log(kettle.plug_in("on"));
 console.log(iron.plug_in("on"));
-
-//Необходимо учитывать мощность включенного электроприбора. Как обратится к множеству объектов???
-
-function power() {
-  //   if(при условии что прибор включен в розетку)
-  return `Cумарная мощность включенных электроприборов ${
-    parseInt(iron.power) + parseInt(kettle.power) + parseInt(lamp.power)
-  } Вт`;
-}
-console.log(power());
-
-
 // сумарная мощность
-// class GeneralPower{
-//     static power(){
-//         if(lamp.plug_in("on")||kettle.plug_in("on")||iron.plug_in("on"))
-
-//     }
-// }
+console.log(GeneralPower.totalPower());
